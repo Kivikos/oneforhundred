c85e770c5e4edf58be44a79fb7fe7f4f2380b932
Konstantin <koc.voytenko@gmail.com>
2021-11-27 10:25:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100; //начальный баланс клиента
+        int add = 1500; //переменная для суммы пополнения
+        int new_balance; //переменная для итогового баланса
+        int cashback; //переменная для бонусов
+
+        if (add > 1000) {
+            cashback = add / 100;
+            new_balance = balance + add + cashback;
+        }
+
+        else {
+            cashback = 0;
+            new_balance = balance + add;
+        }
+        System.out.println("Кэшбек:" + cashback);
+        System.out.println("Ваш баланс:" + new_balance);
+    }
+}
