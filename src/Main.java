ffd7f37aa5885fe09c0cbf02e03c2f85eba3612f
Konstantin Voytenko <koc.voytenko@gmail.com>
2021-12-04 20:30:50 +0300

    camelCase :)

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         int balance = 100; //начальный баланс клиента
         int add = 1500; //переменная для суммы пополнения
-        int new_balance; //переменная для итогового баланса
+        int newBalance; //переменная для итогового баланса
         int cashback; //переменная для бонусов
 
         if (add > 1000) {
             cashback = add / 100;
-            new_balance = balance + add + cashback;
+            newBalance = balance + add + cashback;
         } else {
             cashback = 0;
-            new_balance = balance + add;
+            newBalance = balance + add;
         }
         System.out.println("Кэшбек:" + cashback);
-        System.out.println("Ваш баланс:" + new_balance);
+        System.out.println("Ваш баланс:" + newBalance);
     }
 }
